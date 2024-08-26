483b103bda59ce63a5174dd7788a55caf0c22896
kamran0599 <kamranheyderli049@gmail.com>
2024-08-26 22:30:28 +0400

    add_task

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { MainRouter } from "./routers/router"
-
+import { useEffect, useState } from "react";
+import { MainRouter } from "./routers/router";
+import Spinner from "./components/resuable/Spinner";
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
-  
+  useEffect(() => {
+    // 2 saniyə sonra loading-i false-a dəyişirik
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
 
-  return <MainRouter/>
+    return () => clearTimeout(timer);
+  }, []);
 
+  return loading ? <Spinner /> : <MainRouter />;
 }
 
-export default App
+export default App;
